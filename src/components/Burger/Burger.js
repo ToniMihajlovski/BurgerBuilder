598f7a40955115794dda689eebdccf53d53fd96e
ToniMihajlovski <toni@sopro.io>
2019-11-02 15:50:56 +0100

    using the reduce method

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,20 @@ import classes from './Burger.module.css';
 import BurgerIngridient from './BurgerIngridient/BurgerIngridient';
 
 const burger = (props) => {
-    const transformedIngridients = Object.keys(props.ingredients)
+    let transformedIngridients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_,i) => {       
             return <BurgerIngridient key={igKey + i} type={igKey} />;
         })
-    });
+    },)
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    },[]);
+    if(transformedIngridients.length === 0)
+    {
+        transformedIngridients = <p>Please start adding ingridients</p>
+    }
+    console.log(transformedIngridients);
     return (
         <div className={classes.Burger}>
             <BurgerIngridient type="bread-top"/>
