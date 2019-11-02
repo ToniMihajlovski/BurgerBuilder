0ea9cb2155984b28c7088b1668e1a664af2ca535
ToniMihajlovski <toni@sopro.io>
2019-11-02 18:19:11 +0100

    implementing order now button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,7 +17,22 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+    updatePurchaseState (ingredients) {
+        //  const ingredients = {
+        //      ...this.state.ingredients
+        //  };
+        //to use the updated ingredients not from the state
+         const sum = Object.keys(ingredients)
+         .map(igKey => {
+             return ingredients[igKey];
+         })
+         .reduce((sum, el) => {
+             return sum + el;
+         },0);
+         this.setState({purchasable: sum > 0})
     }
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
@@ -30,6 +45,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     } 
 
     removeIngredientHandler = (type) => {
@@ -47,6 +63,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     }
     render() {
         const disabledInfo = {
@@ -64,6 +81,7 @@ class BurgerBuilder extends Component {
                 ingredientAdded={this.addIngredientHandler}
                 ingredientRemoved={this.removeIngredientHandler}
                 disabled={disabledInfo}
+                purchasable={this.state.purchasable}
                 price={this.state.totalPrice}/>
             </Auxiliary>            
         );
