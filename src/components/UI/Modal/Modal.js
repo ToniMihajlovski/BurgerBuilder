cdc051927faa16e5fdc69fc41e70f1d6803ca7ab
ToniMihajlovski <toni@sopro.io>
2019-11-09 13:31:12 +0100

    Improved App with Should Component Update

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.module.css';
 import Auxiliary from '../../../Auxiliary/Auxiliary';
 import BackDrop from '../BackDrop/BackDrop';
 
-const modal = (props) => (
-    <Auxiliary>
-        <BackDrop show={props.show} clicked={props.modalClosed}/>
-        <div className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Auxiliary>
-);
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show;
+    }
+    componentDidUpdate(){
+        console.log('[Modal] Will Update');
+    }
+    render(){
+        return (
+        <Auxiliary>
+            <BackDrop show={this.props.show} clicked={this.props.modalClosed}/>
+            <div className={classes.Modal}
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}>
+                {this.props.children}
+            </div>
+        </Auxiliary>
+        )
+    }
+};
+
+export default Modal;
