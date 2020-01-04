1ace393c07580736104613520b865d784551ed31
ToniMihajlovski <toni@sopro.io>
2020-01-04 15:26:06 +0100

    Looging Users Out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as actionTypes from './actionTypes';
+import { updateObject } from '../utility';
 
 export const authStart = () => {
   return {
@@ -20,7 +21,23 @@ export const authFail = (error) => {
     type: actionTypes.AUTH_FAIL,
     error: error
   }
-}
+};
+
+
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+
+  }
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+  return dispatch => {
+    setTimeout(() => {
+       dispatch(logout());
+    }, expirationTime * 1000)
+  };
+};
 
 export const auth = (email, password, isSignUp) => {
   return dispatch => {
@@ -38,6 +55,7 @@ export const auth = (email, password, isSignUp) => {
     .then(response => {
       console.log(response);
       dispatch(authSuccess(response.data.idToken, response.data.localId));
+      dispatch(checkAuthTimeout(response.data.expiresIn));
     })
     .catch(err => {
       console.log(err.response);
