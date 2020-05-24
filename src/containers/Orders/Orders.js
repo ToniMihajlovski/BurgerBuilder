2df88742d24629af974d450179d1b3b2271445fe
ToniMihajlovski <toni@sopro.io>
2020-05-24 13:06:30 +0200

    Adding the Order & Checkout Container

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
@@ -6,17 +6,16 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
-class Orders extends Component {
+const Orders = props => {
 
-  componentDidMount() {
-    this.props.onFetchedOrders(this.props.token, this.props.userId);
-  }
+  useEffect(() => {
+    props.onFetchedOrders(props.token, props.userId);
+  }, [])
 
-  render() {
     let orders = <Spinner />;
 
-    if (!this.props.loading) {
-      orders = this.props.orders.map(order =>
+    if (!props.loading) {
+      orders = props.orders.map(order =>
         <Order key={order.id}
           ingredients={order.ingredients}
           price={order.price} />
@@ -27,7 +26,6 @@ class Orders extends Component {
         {orders}
       </div>
     )
-  };
 };
 
 const mapStateToProps = state => {
